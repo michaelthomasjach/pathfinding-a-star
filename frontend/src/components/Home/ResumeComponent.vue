dd0e31430ce2cc796180cf7353df876c877ac11e
michael-jach <michael.jach@octo.com>
2022-06-06 14:07:23 +0200

    build resume

--- frontend/src/components/Home/ResumeComponent.vue
+++ frontend/src/components/Home/ResumeComponent.vue
@@ -1,13 +1,156 @@
 <template>
-  <section class="">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates velit tempore at, cum odio
-    laudantium aperiam labore vel quas quo illum culpa veniam, alias aliquid, dolor error facere
-    nulla magnam modi minima sed ipsum eos laborum. Animi nihil aut sunt doloribus necessitatibus
-    consequuntur accusamus dolorum iusto modi voluptatum voluptatibus, consectetur ipsam quis alias
-    labore iste veniam sed. Nihil laudantium ipsa iure tempora rerum quaerat voluptatem blanditiis
-    quo exercitationem sequi alias reprehenderit qui, accusantium numquam obcaecati at minus. Est
-    non odit consequuntur sit, iure voluptatum, tempora exercitationem temporibus adipisci expedita
-    asperiores. Commodi nihil laboriosam voluptas autem aliquid pariatur placeat molestiae ad.
+  <section class="p-b-30 p-t-40 bg-master-lighter">
+    <div class="container">
+      <div class="wrapper bg-cream p-l-70 p-r-70 p-b-70 p-t-70">
+        <div class="row padding-15 m-b-50">
+          <section>
+            <div class="name m-b-30">
+              <h1 class="">Michael Jach</h1>
+              <h6 class="block-title hint-text no-margin">Développeur</h6>
+            </div>
+            <p>
+              Avide de nouvelle connaissances j'aime découvrir de nouvelles technologies. En 2015
+              j'ai créé en parallèle de mes études une entreprise de communication et de
+              développement web.
+            </p>
+          </section>
+        </div>
+
+        <div class="row">
+          <section class="col-sm-4">
+            <div class="m-b-50">
+              <h2 class="m-t-30 p-b-5">Etude</h2>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cumque sit beatae
+                perspiciatis! Explicabo, asperiores? Minima accusamus expedita sequi nisi!
+              </p>
+            </div>
+
+            <div class="m-t-50">
+              <h2 class="m-t-30 p-b-5">Compétences</h2>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">VueJS</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">React</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Angular</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">NodeJS</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Java Spring</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+            </div>
+          </section>
+          <section class="col-sm-offset-1 col-sm-7">
+            <h2 class="m-t-30 m-b-30">Expérience</h2>
+            <div class="m-b-40 relative">
+              <h6 class="block-title text-black">OCTO Technology - DevOps</h6>
+              <p class="hint-text font-arial small-text p-b-15">
+                ReactJS / NodeJS / Ansible / Git / Gitlab CI / Docker / Cypress / Chai / Typescript
+              </p>
+              <p>
+                Travaille actuellement pour un projet de la DGAC (Direction Générale de l'Aviation
+                Civile) avec l'objectif de développer un produit lié au renouvellement du message
+                ATIS de manière automatique (génération automatique de la voix avec une synthèse
+                vocale) qui sera émis sur les ondes radio. Ce message est ensuite écouté par les
+                aéronefs (avion, hélicoptère, ...) dans le but d'obtenir des informations sur
+                l'aérodrome sur lequel ils souhaitent atterrir.
+              </p>
+              <br />
+              <p>
+                Ce projet à pour ambition de remplacer un ancien logiciel vieux de plus de 30 ans
+                utilisé dans les tours de contrôles. Manipulant des données (In Flight) critiques,
+                ce projet s'inscrit dans un contexte fortement réglementé (SWAL4), demandant:
+              </p>
+              <ul class="align-dots">
+                <li>une qualité de code importante (test e2e, test unitaire, test cypress, ...)</li>
+                <li>
+                  preuves documentaires (schémas d'architectures, logicielles, technique, DSL, DSS,
+                  ...)
+                </li>
+              </ul>
+            </div>
+            <div class="separator m-t-20 m-b-20"></div>
+            <div class="m-t-40 m-b-40 relative">
+              <h6 class="block-title text-black">We Plus - Développeur</h6>
+              <p class="hint-text font-arial small-text p-b-15">
+                Angular / React / VueJS / Docker / Terraform / D3js
+              </p>
+              <p>
+                Pour le compte de Navblue, filiale d'Airbus, j'ai travaillé dans le développement
+                d'un outil web dédié à la détection / prévention d'anomalies sur les vols des
+                compagnies aériennes.
+              </p>
+            </div>
+            <div class="separator m-t-20 m-b-20"></div>
+            <div class="m-t-40 m-b-40 relative">
+              <h6 class="block-title text-black">Elium - Directeur Général</h6>
+              <p class="hint-text font-arial small-text p-b-15">Wordpress / Prestashop</p>
+              <p>
+                Mise en application directe de ma formation, je suis en charge de la conception,
+                réalisation et maîtrise des projets informatiques au sein de l'agence que j'ai
+                fondée (technologies web principalement).
+              </p>
+              <p>
+                En charge de la gestion de l'entreprise, j'ai appris aux fils des années les
+                différentes techniques de développement d'entreprise, les techniques de ventes, le
+                suivi de clientèle ainsi que la mise en place de partenariats.
+              </p>
+            </div>
+            <div class="separator m-t-20 m-b-20"></div>
+            <div class="m-t-40 m-b-40 relative">
+              <h6 class="block-title text-black">Office Formation</h6>
+              <p class="hint-text font-arial small-text p-b-15">
+                Autocad / Revit Architecture / Wordpress
+              </p>
+              <p>
+                Enseignement personnalisé individuel ou en groupe sur Autocad, Revit Architecture,
+                ...
+              </p>
+            </div>
+          </section>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 <script lang="ts">
@@ -26,10 +169,31 @@ export default class ResumeComponent extends Vue {
 </script>
 
 <style lang="scss">
+$dark-color: #212121;
+$cream-color: #fff;
 .bg-image {
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   background-image: url("@/assets/images/hero_mount.jpg");
+}
+.wrapper {
+  &.bg-cream {
+    background-color: $cream-color;
+  }
+  .name {
+    text-align: center;
+  }
+  .align-dots {
+    padding-left: 17px;
+  }
+  .separator {
+    position: relative;
+    display: block;
+    height: 1px;
+    background: #626262;
+    width: 15%;
+    opacity: 0.5;
+  }
 }
 </style>
