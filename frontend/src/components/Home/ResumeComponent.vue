4f9f3524af70b7929b3833dcb6b4f4aceb136d7e
michael-jach <michael.jach@octo.com>
2022-06-06 14:48:38 +0200

    add content to resume

--- frontend/src/components/Home/ResumeComponent.vue
+++ frontend/src/components/Home/ResumeComponent.vue
@@ -19,11 +19,37 @@
         <div class="row">
           <section class="col-sm-4">
             <div class="m-b-50">
-              <h2 class="m-t-30 p-b-5">Etude</h2>
-              <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum cumque sit beatae
-                perspiciatis! Explicabo, asperiores? Minima accusamus expedita sequi nisi!
-              </p>
+              <h2 class="m-t-30 m-b-30">Etudes</h2>
+              <div class="m-b-40 relative">
+                <h6 class="block-title text-black">Ingénieur - CESI - 2013/2018</h6>
+                <p>
+                  Diplôme d'un titre d'ingénieur, j'ai étudié pendant 5 ans les différents aspects
+                  des métiers de l'informatique ; développement, architecture, documentation
+                  techinique, ...
+                </p>
+                <p class="hint-text font-arial small-text p-b-15">
+                  Association 3D - Réalisations de cour métrages, montages vidéos, site internet
+                  voués à la promotions de l'association en passant par la création de Flyers pour
+                  des événements, ...
+                </p>
+              </div>
+              <div class="m-b-40 relative">
+                <h6 class="block-title text-black">BTS GC - Louis Vicat - 2010/2012</h6>
+                <p>
+                  Diplôme d'un BTS en génie civil, l'informatique n'a pas été mon unique intérêt,
+                  autrefois l'architecture été un domaine qui me passionnait.
+                </p>
+                <p class="hint-text font-arial small-text p-b-15">
+                  Dessin technique Autocad, Revit, calcul de descente de charge, topographie ...
+                </p>
+              </div>
+              <div class="m-b-40 relative">
+                <h6 class="block-title text-black">BAC STI GC - Louis Vicat - 2007/2010</h6>
+                <p>Etude spécialisée dans le domaine du génie civil.</p>
+                <p class="hint-text font-arial small-text p-b-15">
+                  Etudes des sols, résistances des matériaux, ...
+                </p>
+              </div>
             </div>
 
             <div class="m-t-50">
@@ -78,12 +104,128 @@
                   <i class="fa fa-star-o"></i>
                 </div>
               </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Git</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Ansible</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Terraform</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Docker</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">HTML/CSS/JS</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="m-t-50">
+              <h2 class="m-t-30 p-b-5">Langues</h2>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Francais</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Anglais</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-6 p-l-0">Espagnol</div>
+                <div class="col-sm-6 text-right">
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star"></i>
+                  <i class="fa fa-star-o"></i>
+                  <i class="fa fa-star-o"></i>
+                </div>
+              </div>
+            </div>
+
+            <div class="m-t-50">
+              <h2 class="m-t-30 p-b-5">Certifications</h2>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-10 p-l-0">AWS Associate Developper</div>
+                <div class="col-sm-2 text-right">?</div>
+              </div>
+              <div class="m-b-10 p-b-20">
+                <div class="col-sm-10 p-l-0">TOEIC</div>
+                <div class="col-sm-2 text-right">810</div>
+              </div>
+            </div>
+
+            <div class="m-t-50">
+              <h2 class="m-t-30 p-b-5">Autres</h2>
+              <div class="m-b-40 relative">
+                <h6 class="block-title text-black">Youtube - 60000 abonnés</h6>
+                <p>
+                  Reconnu dans la communauté française comme étant l'un des plus gros contributeur
+                  de contenu sur la formation de logiciel liés à la DAO.
+                  <br/>
+                </p>
+                <p class="hint-text font-arial small-text m-b-0">
+                  Suite Autodesk, Photoshop, After Effect, Lumion, ...
+                </p>
+                <p class="hint-text font-arial small-text p-b-15">
+                  https://www.youtube.com/c/cyborggraphics
+                </p>
+              </div>
             </div>
           </section>
+
           <section class="col-sm-offset-1 col-sm-7">
-            <h2 class="m-t-30 m-b-30">Expérience</h2>
+            <h2 class="m-t-30 m-b-30">Expériences</h2>
             <div class="m-b-40 relative">
               <h6 class="block-title text-black">OCTO Technology - DevOps</h6>
+              <p class="hint-text font-arial small-text m-b-0">avr. 2021 - aujourd'hui</p>
               <p class="hint-text font-arial small-text p-b-15">
                 ReactJS / NodeJS / Ansible / Git / Gitlab CI / Docker / Cypress / Chai / Typescript
               </p>
@@ -109,9 +251,14 @@
                 </li>
               </ul>
             </div>
+
             <div class="separator m-t-20 m-b-20"></div>
+
             <div class="m-t-40 m-b-40 relative">
               <h6 class="block-title text-black">We Plus - Développeur</h6>
+              <p class="hint-text font-arial small-text m-b-0">
+                févr. 2018 - mars 2021 - (3 ans 2 mois)
+              </p>
               <p class="hint-text font-arial small-text p-b-15">
                 Angular / React / VueJS / Docker / Terraform / D3js
               </p>
@@ -121,9 +268,14 @@
                 compagnies aériennes.
               </p>
             </div>
+
             <div class="separator m-t-20 m-b-20"></div>
+
             <div class="m-t-40 m-b-40 relative">
               <h6 class="block-title text-black">Elium - Directeur Général</h6>
+              <p class="hint-text font-arial small-text m-b-0">
+                avr. 2015 - janv. 2018 - (2 ans 10 mois)
+              </p>
               <p class="hint-text font-arial small-text p-b-15">Wordpress / Prestashop</p>
               <p>
                 Mise en application directe de ma formation, je suis en charge de la conception,
@@ -136,15 +288,34 @@
                 suivi de clientèle ainsi que la mise en place de partenariats.
               </p>
             </div>
+
             <div class="separator m-t-20 m-b-20"></div>
+
             <div class="m-t-40 m-b-40 relative">
               <h6 class="block-title text-black">Office Formation</h6>
+              <p class="hint-text font-arial small-text m-b-0">
+                sept. 2015 - févr. 2018 - (2 ans 6 mois)
+              </p>
               <p class="hint-text font-arial small-text p-b-15">
                 Autocad / Revit Architecture / Wordpress
               </p>
               <p>
                 Enseignement personnalisé individuel ou en groupe sur Autocad, Revit Architecture,
                 ...
+              </p>
+            </div>
+
+            <div class="separator m-t-20 m-b-20"></div>
+
+            <div class="m-t-40 m-b-40 relative">
+              <h6 class="block-title text-black">Freelance - Développeur Front-end</h6>
+              <p class="hint-text font-arial small-text p-b-15">
+                avr. 2015 - juil. 2015 - (4 mois)
+              </p>
+              <p>
+                Création d'une auto-entreprise de développement web (langages web liés au
+                front-end). Je me suis initié au monde de l'entreprise par le biais de partenaires
+                de travail qui m'ont appris les bases de l'entrepreneuriat.
               </p>
             </div>
           </section>
